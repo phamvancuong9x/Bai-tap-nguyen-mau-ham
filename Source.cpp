83e4fa82e876e27ca66d85cebc0043e10c70e22e
phamvancuong9x <80909286+phamvancuong9x@users.noreply.github.com>
2021-03-25 12:51:04 +0700

    Add files via upload
    so dao

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+﻿////NHAP vao so x ,in ra so dao cua x
+
+#include<iostream>
+using namespace std;
+int daoso();
+int main(){
+cout<<daoso();
+cout << endl;
+	system("pause");
+	return 0;
+}
+int daoso() {
+    int n, tong = 0, term;
+    cin >> n;
+   
+    while (n != 0)
+    {
+        term = n% 10;
+        tong = tong * 10 + term;
+        n/= 10;
+    }
+	return tong;
+}
